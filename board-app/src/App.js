0928f5b61726dc482c1a992eec8f4895034e7919
ebinaha <ebinha012@gmail.com>
2022-11-23 14:57:33 +0900

    board file-upload

---
+++ board-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import WriteBoard from './component/WriteBoard';
+
+function App() {
+  return (
+    <div>
+        <WriteBoard/>
+    </div>
+  );
+}
+
+export default App;
---
+++ board-app/src/component/WriteBoard.js
@@ -0,0 +1,133 @@
+import {Component} from 'react';
+import './WriteBoard.css';
+import axios from 'axios';
+
+class WriteBoard extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            writer : '',
+            password : '',
+            subject : '',
+            content : '',
+            file : null,
+
+        }
+    }
+
+    change = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        this.setState({[name]:value});
+    }
+
+    submit = (e) => {
+        // form 태그 안에 있기 때문에 submit 기능을 막아주는 것이 필요
+        e.preventDefault();
+        // JS제공 객체 : FormData
+        const formData = new FormData();
+        formData.append('writer', this.state.writer);
+        formData.append('password', this.state.password);
+        formData.append('subject', this.state.subject);
+        formData.append('content', this.state.content);
+        formData.append('file', this.state.file);
+
+        // const config = {
+        //     headers : {
+        //         'content-type' : 'multipart/form-data'  //반드시 컨텐트 타입 설정은 멀티파트로 해야 함
+        //     }
+        // }
+
+        axios.post('http://localhost:8090/writeboard', formData)
+        .then((response) => {
+            alert(response.data);
+
+        }).catch((err) => {
+
+        })
+    }
+
+    fileChange = (e) => {
+        // 멀티파일이 가능?
+        this.setState({file : e.target.files[0]})
+    }
+
+    render(){
+        return(
+            <section>
+                <h2> 게시판 </h2>
+                <form>
+                    <table>
+                        <tr>
+                            <td className='td_left'>
+                                <label for='writer'>
+                                    글쓴이
+                                </label>
+                            </td>
+                            <td className='td_right'>
+                                <input type='text' name='writer' id='writer' value={this.state.writer} onChange={this.change}/> 
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td className='td_left'>
+                                <label for='password'>
+                                    비밀번호
+                                </label>
+                            </td>
+                            <td className='td_right'>
+                                <input type='password' name='password' id='password' value={this.state.password} onChange={this.change}/> 
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td className='td_left'>
+                                <label for='subject'>
+                                    제목
+                                </label>
+                            </td>
+                            <td className='td_right'>
+                                <input type='text' name='subject' id='subject' value={this.state.subject} onChange={this.change}/>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td className='td_left'>
+                                <label for='content'>
+                                    내용
+                                </label>
+                            </td>
+                            <td className='td_right'>
+                                <textarea name='content' id='content' cols='40' rows='15' value={this.state.content} onChange={this.change}>
+                                </textarea>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td className='td_left'>
+                                <label for='file'>
+                                    파일첨부
+                                </label>
+                            </td>
+                            <td className='td_right'>
+                                {/* file은 value대신 함수로 변수 연동 */}
+                                <input type='file' name='file' id='file' onChange={this.fileChange}/>
+                            </td>
+                        </tr>
+                    </table>
+
+                    <section id="commandCell">
+                        <button onClick={this.submit}> 등록 </button> 
+                        &nbsp; &nbsp;
+                        <input type='reset' value='다시쓰기'/>
+                    </section>
+                </form>
+                
+            </section>
+        )
+    }
+}
+
+export default WriteBoard;
+
