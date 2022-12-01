fd5691bc932f2c35570d61f1fd36647ad698ad5d
ebinaha <ebinha012@gmail.com>
2022-12-01 11:31:50 +0900

    openAPI - recharts  Line Chart

--- openapi-app/src/App.js
+++ openapi-app/src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import SubwayApi from './SubwayApi';
+import RechartsLine from './RechartsLine';
 
 function App() {
   return (
     <div className="App">
-
+      <RechartsLine/>
       <SubwayApi/>
+      
     </div>
   );
 }
--- openapi-app/src/SubwayApi.js
+++ openapi-app/src/SubwayApi.js
@@ -6,7 +6,7 @@ function SubwayApi(){
     const [row, setRow] = useState([]);
 
     const apiRequest = () => {
-        axios.get("http://openapi.seoul.go.kr:8088/79415655796265613130364b44784474/json/CardSubwayStatsNew/1/10/20221019")
+        axios.get("http://openapi.seoul.go.kr:8088/79415655796265613130364b44784474/json/CardSubwayStatsNew/1/50/20221019")
         .then((response) => {
             console.log(response.data)
             // 사용할 배열만 가져오기
@@ -23,7 +23,7 @@ function SubwayApi(){
             <br/>
             <Button color="success" outline onClick={apiRequest}> 데이터 가져오기 </Button>
             <br/><br/>
-            <Table>
+            <Table style={{width:"600px", margin:"0 auto"}}>
                 <thead>
                     <tr className='table-success'>
                         <th> 날짜 </th>
