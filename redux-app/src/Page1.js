e51ef170f8b6718e1426ba4a90fc83d7590ba0ec
ebinaha <ebinha012@gmail.com>
2022-12-09 17:30:12 +0900

    redux action 데이터 변경

--- redux-app/src/Page1.js
+++ redux-app/src/Page1.js
@@ -1,6 +1,23 @@
+import {useSelector, useDispatch} from 'react-redux';
+import Page2 from './Page2';
+
 
 function Page1(){
+    // 함수의 매개변수로 useState를 넘겨줌 => data1을 리턴
+    const data1 = useSelector(state=>state.data1);
 
+    // useDispatch : reducer 함수 호출
+    const dispatch = useDispatch();
+
+    return (
+        <>
+            <h1> {data1} </h1>
+            {/* state는 빼고 action만 넘김 : type, value => 내가 만든 객체는 reducer의 action으로 감 */}
+            <button onClick={()=>{dispatch({type:"STRING", data:"Hong"})}}> change string </button>
+            <Page2/>
+        </>
+    )
+    
 }
 
 export default Page1;
---
+++ redux-app/src/Page2.js
@@ -0,0 +1,22 @@
+import {useSelector, useDispatch} from 'react-redux';
+
+function Page2(){
+    const data1 = useSelector(state=>state.data1);
+    const data2 = useSelector(state=>state.data2);
+
+    const dispatch = useDispatch();
+
+    const click = () => {
+        dispatch({type:"INT", data:100});
+    }
+
+    return(
+        <>
+            <h2> {data1} </h2>
+            <h2> {data2} </h2>
+            <button onClick={click}> change number </button>
+        </>
+    )
+}
+
+export default Page2;
