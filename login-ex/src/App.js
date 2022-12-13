21d4b7ee144a50d774375249ea62f58d861e69ea
ebinaha <ebinha012@gmail.com>
2022-12-13 09:51:47 +0900

    app.js Redux 생성

--- login-ex/src/App.js
+++ login-ex/src/App.js
@@ -2,17 +2,43 @@
 import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
+// 컴포넌트 분리 가능
+function reducer(currentState, action){
+  if(currentState == undefined){
+    return({
+      // 토큰
+      Authorization:'',
+      UserId:''
+    })
+  }
+  const newState = {...currentState};
+  switch(action.type) {
+    case "NEWTOKEN" : newState.Authorization = action.data;
+    break;
+    case "USERID" : newState.UserId = action.data;
+  }
+  return newState;
+}
+
+const store = createStore(reducer);
+
+
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-      {/* 네비게이션 */}
-      <Header/>
-        <Routes>
-          <Route exact path='/' element={<Login/>}/>
-          <Route exact path='/user' element={<User/>}/>
-        </Routes>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+        {/* 네비게이션 */}
+        <Header/>
+          <Routes>
+            <Route exact path='/' element={<Login/>}/>
+            <Route exact path='/user' element={<User/>}/>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
